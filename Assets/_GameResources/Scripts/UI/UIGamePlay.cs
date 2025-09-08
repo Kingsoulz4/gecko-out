69d9dd1789c6d19067d8c91e7744b27ee9fa1b29
dathv <dat_hoang@git>
2025-09-09 00:16:45 +0700

    Update

--- Assets/_GameResources/Scripts/UI/UIGamePlay.cs
+++ Assets/_GameResources/Scripts/UI/UIGamePlay.cs
@@ -11,10 +11,20 @@ namespace Geckout
     public class UIGamePlay : MonoBehaviour
     {
         [SerializeField] private Button m_designLevel;
+        [SerializeField] private TextMeshProUGUI m_textTime;
 
         private void Awake()
         {
             m_designLevel.onClick.AddListener(OnClickDesignLevel);
+        }
+
+        private void Update()
+        {
+            if (GamePlayManager.Instance.LevelGame != null && GamePlayManager.Instance.LevelGame.GameLevelData != null)
+            {
+                var timeSecond = GamePlayManager.Instance.LevelGame.GameLevelData.time;
+                m_textTime.text = $"{timeSecond / 60}:{timeSecond % 60}";
+            }
         }
 
         private void OnClickDesignLevel()
