31ba5318f978aab3330261e60c7c462c32cd7abb
daiht <daiht@android.vn>
2025-09-09 16:26:01 +0700

    rgt

--- Assets/_GameResources/Scripts/GamePlay/Body/OccupiedTileController.cs
+++ Assets/_GameResources/Scripts/GamePlay/Body/OccupiedTileController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Geckout
@@ -27,7 +28,6 @@ namespace Geckout
 
         private void OnEndMoveHandle()
         {
-            UpdateAllSegmentPositions();
             ForceRestoreAll();
         }
 
