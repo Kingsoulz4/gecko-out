92a1f4bb3b819b426eff5ec3a17823fe6dcabaf1
dathv <dat_hoang@git>
2025-09-11 00:38:15 +0700

    Init Ice

---
+++ Assets/_GameResources/Scripts/GamePlay/Machanics/Ice/IceRenderer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geckout
+{
+    public class IceRenderer : MonoBehaviour
+    {
+        [SerializeField] private IceTile m_icePrefab;
+
+        public void GenerateIces(List<Vector3> listPosition)
+        {
+
+        }
+    }
+}
