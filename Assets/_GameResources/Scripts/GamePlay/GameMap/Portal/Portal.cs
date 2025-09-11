983c93460ba65966fa930119af81965742f213cc
daiht <daiht@android.vn>
2025-09-11 10:45:00 +0700

    rft

--- Assets/_GameResources/Scripts/GamePlay/GameMap/Portal/Portal.cs
+++ Assets/_GameResources/Scripts/GamePlay/GameMap/Portal/Portal.cs
@@ -10,8 +10,7 @@ namespace Geckout
     public class Portal : MonoBehaviour
     {
         [SerializeField] private List<BodyPartColorChanger> bodyPartColorChangers = new();
-
-        public PortalData PortalData { get; set; }
+        public PortalData PortalData { get; private set; }
 
         private void Awake()
         {
