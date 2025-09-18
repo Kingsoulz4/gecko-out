51ff582229282a9955e8734822748ec8f362f472
daiht <daiht@android.vn>
2025-09-18 15:23:45 +0700

    rft

--- Assets/_GameResources/Scripts/NewFeature/NewFeatureManager.cs
+++ Assets/_GameResources/Scripts/NewFeature/NewFeatureManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewFeatureManager : Singleton<NewFeatureManager>
+public class NewFeatureManager : SingletonMono<NewFeatureManager>
 {
     [SerializeField] private NewFeatureSO newFeatureSO;
 
-    public NewFeatureSO NewFeatureSO { get => newFeatureSO;}
+    public NewFeatureSO NewFeatureSO { get => newFeatureSO; }
 }
