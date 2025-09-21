8a9273cecf642a4c8385fe84a5e05426e6e7a95f
daiht <daiht@android.vn>
2025-09-21 11:09:26 +0700

    select body icon

--- Assets/_GameResources/Scripts/GamePlay/Booster/Item/BodySelected.cs
+++ Assets/_GameResources/Scripts/GamePlay/Booster/Item/BodySelected.cs
@@ -11,24 +11,45 @@ namespace Geckout
     {
         [SerializeField] private float moveDistance = 0.5f;
         [SerializeField] private float duration = 0.5f;
+        [SerializeField] private Transform visual;
         private Quaternion originalRotation = Quaternion.identity;
         private Vector3 tmpPos = new Vector3();
-
+        private Tween tween;
 
         private void LateUpdate()
         {
             transform.rotation = originalRotation;
-            tmpPos = transform.parent.position; 
+            tmpPos = transform.parent.position;
             tmpPos.z = -1;
             tmpPos.y += 0.5f;
             transform.position = tmpPos;
-        }   
+        }
+
+        private void OnEnable()
+        {
+            DoEffect();
+        }
+
+        private void OnDisable()
+        {
+            if (tween != null)
+            {
+                tween.Kill();
+            }
+        }
 
         public void DoEffect()
         {
-           float startY = transform.position.y;
-            transform.DOMoveY(startY + moveDistance, duration).
-                SetLoops(-1, DG.Tweening.LoopType.Yoyo).SetEase(DG.Tweening.Ease.InOutSine);
+            float startY = visual.localPosition.y;
+            if (tween != null)
+            {
+                tween.Kill();
+            }
+
+            var sq = DOTween.Sequence();
+
+            sq.Append(visual.DOLocalMoveY(startY + moveDistance, duration).
+                SetLoops(-1, DG.Tweening.LoopType.Yoyo).SetEase(DG.Tweening.Ease.InOutSine));
         }
     }
 }
