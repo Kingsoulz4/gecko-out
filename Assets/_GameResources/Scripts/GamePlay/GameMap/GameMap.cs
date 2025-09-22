78d4859960042d7e023eae4853c738e0f601421a
dathv <dathv@android.vn>
2025-09-22 15:41:53 +0700

    Update GameMap.cs

--- Assets/_GameResources/Scripts/GamePlay/GameMap/GameMap.cs
+++ Assets/_GameResources/Scripts/GamePlay/GameMap/GameMap.cs
@@ -424,41 +424,53 @@ namespace Geckout
             var wallCornerPrefab = m_tileWallCorner;
             var wallEdgePrefab = m_tileWallEdge;
 
-            var angleCornerBottomLeft = new Vector3(270, -90, 90);
-            PlaceWall(wallCornerPrefab, new Vector2Int(-1, -1), cellSize, cubeSize, centerOffset, "Corner_BottomLeft", angleCornerBottomLeft);
-            var angleCornerBottomRight = new Vector3(0, -90, 90);
-            PlaceWall(wallCornerPrefab, new Vector2Int(gridSize.x, -1), cellSize, cubeSize, centerOffset, "Corner_BottomRight", angleCornerBottomRight);
-            var angleCornerTopLeft = new Vector3(0, 90, -90);
-            PlaceWall(wallCornerPrefab, new Vector2Int(-1, gridSize.y), cellSize, cubeSize, centerOffset, "Corner_TopLeft", angleCornerTopLeft);
-            var angleCornerTopRight = new Vector3(90, -90, 90);
-            PlaceWall(wallCornerPrefab, new Vector2Int(gridSize.x, gridSize.y), cellSize, cubeSize, centerOffset, "Corner_TopRight", angleCornerTopRight);
+            //var angleCornerBottomLeft = new Vector3(270, -90, 90);
+            //PlaceWall(wallCornerPrefab, new Vector2Int(-1, -1), cellSize, cubeSize, centerOffset, "Corner_BottomLeft", angleCornerBottomLeft);
+            //var angleCornerBottomRight = new Vector3(0, -90, 90);
+            //PlaceWall(wallCornerPrefab, new Vector2Int(gridSize.x, -1), cellSize, cubeSize, centerOffset, "Corner_BottomRight", angleCornerBottomRight);
+            //var angleCornerTopLeft = new Vector3(0, 90, -90);
+            //PlaceWall(wallCornerPrefab, new Vector2Int(-1, gridSize.y), cellSize, cubeSize, centerOffset, "Corner_TopLeft", angleCornerTopLeft);
+            //var angleCornerTopRight = new Vector3(90, -90, 90);
+            //PlaceWall(wallCornerPrefab, new Vector2Int(gridSize.x, gridSize.y), cellSize, cubeSize, centerOffset, "Corner_TopRight", angleCornerTopRight);
 
             // Bottom edge
             var angleBottomEdge = new Vector3(180, 90, -90);
             for (int x = 0; x < gridSize.x; x++)
-                PlaceWall(wallEdgePrefab, new Vector2Int(x, -1), cellSize, cubeSize, centerOffset, $"Wall_Bottom_{x}", angleBottomEdge);
+                PlaceWall(wallEdgePrefab, new Vector2Int(x, -1), cellSize, cubeSize, centerOffset, $"Wall_Bottom_{x}", angleBottomEdge, Vector3.down);
 
             // Top edge
             var angleTopEdge = new Vector3(0, 90, -90);
             for (int x = 0; x < gridSize.x; x++)
-                PlaceWall(wallEdgePrefab, new Vector2Int(x, gridSize.y), cellSize, cubeSize, centerOffset, $"Wall_Top_{x}", angleTopEdge);
+                PlaceWall(wallEdgePrefab, new Vector2Int(x, gridSize.y), cellSize, cubeSize, centerOffset, $"Wall_Top_{x}", angleTopEdge, Vector3.up);
 
             // Left edge
             var angleLeftEdge = new Vector3(270, -90, 90);
             for (int y = 0; y < gridSize.y; y++)
-                PlaceWall(wallEdgePrefab, new Vector2Int(-1, y), cellSize, cubeSize, centerOffset, $"Wall_Left_{y}", angleLeftEdge);
+                PlaceWall(wallEdgePrefab, new Vector2Int(-1, y), cellSize, cubeSize, centerOffset, $"Wall_Left_{y}", angleLeftEdge, Vector3.left);
 
             // Right edge
             var angleRightEdge = new Vector3(90, -90, 90);
             for (int y = 0; y < gridSize.y; y++)
-                PlaceWall(wallEdgePrefab, new Vector2Int(gridSize.x, y), cellSize, cubeSize, centerOffset, $"Wall_Right_{y}", angleRightEdge);
+                PlaceWall(wallEdgePrefab, new Vector2Int(gridSize.x, y), cellSize, cubeSize, centerOffset, $"Wall_Right_{y}", angleRightEdge, Vector3.right);
         }
 
-        private void PlaceWall(GameTile prefab, Vector2Int c, float cellSize, float cubeSize, Vector3 centerOffset, string name, Vector3 localRotation)
+        private void PlaceWall(GameTile prefab, Vector2Int c, float cellSize, float cubeSize, Vector3 centerOffset, string name, Vector3 localRotation, Vector3 dir)
         {
             if (prefab == null) return;
 
             Vector3 pos = new Vector3(c.x * cellSize, c.y * cellSize, 0) - centerOffset;
+
+            var tileData = new MapTileData();
+            tileData.coordinate = c;
+
+            //if (dir == Vector3.left)
+            //{
+            //    GameMap.TryGetTileAtCoord(c + Vector2Int.left, out var tileLeft);
+            //    bool left = !(tileLeft == null || tileLeft.MapTileData.type != MapTileType.Normal || listCoordTileSelected.Contains(tileLeft));
+            //}
+            //GameMap.TryGetTileAtCoord(tile.Coordinate + Vector2Int.right, out var tileRight);
+            //bool right = !(tileRight == null || tileRight.MapTileData.type != MapTileType.Normal || listCoordTileSelected.Contains(tileRight));
+
 
             GameObject obj;
 #if UNITY_EDITOR
