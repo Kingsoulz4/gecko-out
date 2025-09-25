756e55ed957e6ad17247e11cf810b3b066081697
Van Dat - Unity Developer <dathv@AS.LOCAL>
2025-09-25 15:45:00 +0700

    UI Shop

---
+++ Assets/_GameResources/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geckout
+{
+    public class ShopManager : Singleton<ShopManager>
+    {
+        public void PurchasePackage(string packID)
+        {
+
+        }
+    }
+}
