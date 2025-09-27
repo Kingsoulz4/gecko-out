1bc8214d2ecfc26c62280a60d0bcaa713f2c7d91
dathv <dat_hoang@git>
2025-09-28 00:13:05 +0700

    Update Receive Reward Fx

--- Assets/_UI/Scripts/UI/Popups/PopupReceiveReward.cs
+++ Assets/_UI/Scripts/UI/Popups/PopupReceiveReward.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ namespace Geckout
         [SerializeField] private ItemRewardUI m_itemPrefab;
         [SerializeField] private List<Transform> m_listRewardHolders;
         [SerializeField] private Button m_buttonClaim;
+        [SerializeField] private GameObject m_ribbon;
 
         private void Awake()
         {
@@ -52,6 +54,39 @@ namespace Geckout
                   
                 }
             }
+            StartCoroutine(IEAnimateShow());
+        }
+
+        private IEnumerator IEAnimateShow()
+        {
+            for(int i=0; i<m_listRewardHolders.Count; i++)
+            {
+                for(int j=0; j < m_listRewardHolders[i].childCount; j++)
+                {
+                    m_listRewardHolders[j].transform.localScale = Vector3.zero;
+                }
+            }
+            m_ribbon.transform.localScale = Vector3.zero;
+            m_buttonClaim.transform.localScale = Vector3.zero;
+
+            yield return new WaitForEndOfFrame();
+
+            m_ribbon.transform.DOScale(1, 0.5f).SetEase(Ease.OutBack);
+
+            yield return new WaitForSeconds(0.5f);
+
+            for (int i = 0; i < m_listRewardHolders.Count; i++)
+            {
+                for (int j = 0; j < m_listRewardHolders[i].childCount; j++)
+                {
+                    m_listRewardHolders[j].transform.DOScale(1, 0.2f).SetEase(Ease.OutBack);
+                    yield return new WaitForSeconds(0.15f);
+                }
+            }
+
+            yield return new WaitForSeconds(0.2f);
+
+            m_buttonClaim.transform.DOScale(1, 0.25f).SetEase(Ease.OutBack);
         }
     }
 }
