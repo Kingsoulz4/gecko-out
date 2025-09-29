adec30ce1bc92d4ac9b10ab6ebe5bbd5e16e12af
dathv <dathv@android.vn>
2025-09-29 14:29:34 +0700

    Update anim dog

---
+++ Assets/_GameResources/Scripts/Common/RandomIntParamAnimator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geckout
+{
+    public class RandomIntParamAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private string paramName;
+        [SerializeField] private int minVal = 0;
+        [SerializeField] private int maxVal = 3;
+        [SerializeField] private float minDelay = 2f;
+        [SerializeField] private float maxDelay = 5f;
+
+        private void Start()
+        {
+            if (animator == null) animator = GetComponent<Animator>();
+            StartCoroutine(PlayRandomAnimationLoop());
+        }
+
+        private IEnumerator PlayRandomAnimationLoop()
+        {
+            while (true)
+            {
+                // Wait random seconds
+                float delay = Random.Range(minDelay, maxDelay);
+                yield return new WaitForSeconds(delay);
+
+                // Pick random animation
+                var val = Random.Range(minVal, maxVal);
+
+                // Play it
+                animator.SetInteger(paramName, val);
+                yield return null;
+                yield return new WaitForSeconds(animator.GetCurrentAnimatorClipInfo(0)[0].clip.length);
+                animator.SetInteger(paramName, 0);
+
+            }
+        }
+    }
+}
